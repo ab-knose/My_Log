32e1fc872f1280be24a1ffb966579fa75687de28
ab-yuyamakawa <yuyamakawa@abeam.com>
2025-06-25 10:06:14 +0900

    App.vue移動

--- vue_env/src/App.vue
+++ vue_env/src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+import { ref } from "vue";
+
+const responseData = ref(null);
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get("http://localhost:5173/"); // URLを適宜変更してください
+    responseData.value = response.data;
+  } catch (error) {
+    console.error(error);
+    responseData.value = "エラーが発生しました";
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <button @click="fetchData">GETリクエスト送信</button>
+  <div v-if="responseData">
+    {{ responseData }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
