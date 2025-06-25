ddf96d062616d64a83b2fb8887c41146d7203405
ab-yuyamakawa <yuyamakawa@abeam.com>
2025-06-25 21:55:57 +0900

    ラベル日付取得＆表示のfunction整理

--- vue_env/src/components/views/calendar.vue
+++ vue_env/src/components/views/calendar.vue
@@ -19,53 +19,55 @@ import axios from 'axios'
 interface CalendarEvent {
   start: string
   end: string
-  //title: string
-  //class: string
 }
 
-//vueカレンダーで必要な情報を渡す
+//vueカレンダーで必要な情報を使えるようにする
 export default defineComponent({
   components: { VueCal },
   setup() {
-    //振り返りを行った日付を取得
-    const labeledData = ref<string[]>([])
-    const getLabeledData = async (user_id: string) => {
-      try {
-        const response = await axios.get(`http://127.0.0.1:8000/chats/labeled_dates/${user_id}`)
-        labeledData.value = response.data
-        console.log('Labeled data:', labeledData.value) 
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    //APIから取得した日付をvueカレンダーで表示するための形式に変換
-    function yearsToEventArray(years: string[]): { start: string; end: string }[] {
-      return years.map(year => ({
-        start: year,
-        end: year
-      }));
-    }
-    const events = ref<CalendarEvent[]>([])
+    const labeledData = ref<string[]>([]) // APIから取得した振り返りを行った日付のリスト
+    const events = ref<CalendarEvent[]>([]) // vueカレンダーで表示するイベントのリスト
+    const selectedDate = ref<Date>(new Date()) // カレンダー上で選択された日付
+    
+    //振り返りを行った日付を取得＆vueカレンダーで表示する形式に変換
     onMounted(async () => {
       //TODO: ユーザーIDをログイン中のユーザのものに変更すること
-      await getLabeledData('user001')
+      labeledData.value = await getLabeledData('user001')
       events.value = yearsToEventArray(labeledData.value)
     })
 
-    const selectedDate = ref<Date>(new Date())
-
-    const onCellClick = ({ date }: { date: string }) => {
+    //日付クリック時の処理
+    const onCellClick = (date: string) => {
       alert(`${date} がクリックされました`)
     }
-    console.log(events.value)
+
     return {
       selectedDate,
       events,
       onCellClick,
     }
+
   }
 })
+
+// APIから振り返りを行った日付を取得する関数
+async function getLabeledData(user_id: string){
+  try {
+    const response = await axios.get(`http://127.0.0.1:8000/chats/labeled_dates/${user_id}`)
+    return response.data 
+  }catch (error) {
+    console.error(error)
+  }
+  }
+
+//APIから取得した日付をvueカレンダーで表示するための形式に変換
+function yearsToEventArray(years: string[]): { start: string; end: string }[] {
+  return years.map(year => ({
+    start: year,
+    end: year
+  }));
+}
+
 </script>
 
 <style>
