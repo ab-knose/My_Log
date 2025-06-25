f476252c182160e28f8d22be41bc2110a47ef85f
ab-kebe <kebe@abeam.com>
2025-06-25 12:51:04 +0900

    CORSミドルウェアを設定した。vueからアクセスできる。

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # FastAPIおよびpydantic
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 # SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
@@ -30,6 +31,17 @@ def get_db_session():
 
 # FastAPIアプリケーションのインスタンスを作成
 app = FastAPI()
+
+# CORSミドルウェアの設定
+# これにより、フロントエンドアプリケーション（例：Vue.jsやReactなど）からのリクエストを許可する。
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # または ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def get_root():
