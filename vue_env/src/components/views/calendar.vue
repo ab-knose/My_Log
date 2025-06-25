b29abfaefbc73fce9aa98a92a9e75f5653c9b5eb
ab-yuyamakawa <yuyamakawa@abeam.com>
2025-06-25 17:07:27 +0900

    非同期オブジェクトによるバグ

--- vue_env/src/components/views/calendar.vue
+++ vue_env/src/components/views/calendar.vue
@@ -1,5 +1,5 @@
 <!-- filepath: c:\fy26-digital-dev-training-handson\group1_dev\My_Log\components\MyCalendar.vue -->
-<template>
+<template>  
   <vue-cal
     style="height: 500px"
     :selected-date="selectedDate"
@@ -14,55 +14,64 @@
 import { defineComponent, ref } from 'vue'
 import VueCal from 'vue-cal'
 import 'vue-cal/dist/vuecal.css'
+import axios from 'axios'
 
 interface CalendarEvent {
   start: string
   end: string
-  title: string
-  class: string
+  //title: string
+  //class: string
 }
 
 export default defineComponent({
   components: { VueCal },
   setup() {
+    const getLabeledData = async (user_id: string) => {
+      try {
+        const response = await axios.get(`http://127.0.0.1:8000/chats/labeled_dates/${user_id}`)
+        return response.data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    const labeledData = getLabeledData('user001')
+    function yearsToEventArray(years: string[]): { start: string; end: string }[] {
+      return years.map(year => ({
+        start: year,
+        end: year
+      }));
+    }
+    console.log(labeledData)
+    //console.log(yearsToEventArray(labeledData))
+    //console.log(yearsToEventArray(['2025-06-21', '2025-06-15']))
+    
+    const onCellClick = ({ date }: { date: string }) => {
+      alert(`${date} がクリックされました`)
+    }
+
     const selectedDate = ref<Date>(new Date())
     const events = ref<CalendarEvent[]>([
       {
         start: '2025-06-21',
         end: '2025-06-21',
-        title: '★マーク',
-        class: 'marked-event'
+        //title: '★マーク',
+        //class: 'marked-event'
       },
       {
         start: '2025-06-15',
         end: '2025-06-15',
-        title: '✔',
-        class: 'marked-event'
+        //title: '✔',
+        //class: 'marked-event'
       }
     ])
-
-    const onCellClick = ({ date }: { date: string }) => {
-      alert(`${date} がクリックされました`)
-    }
 
     return {
       selectedDate,
       events,
-      onCellClick
+      onCellClick,
     }
   }
 })
-
-const getLabeledData = async () => {
-  try {
-    const response = await axios.get("http://127.0.0.1:8000/chats/single/user001"); // URLを適宜変更してください
-    responseData.value = response.data;
-  } catch (error) {
-    console.error(error);
-    responseData.value = "エラーが発生しました";
-  }
-  return responseData.value;
-};
 </script>
 
 <style>
